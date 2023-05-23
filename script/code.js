e59d306c9afbd98d4c4752183c3f2d33bad28a62
Uthmaan <samsodienuthmaan@gmail.com>
2023-05-23 16:50:45 +0200

    first push

---
+++ script/code.js
@@ -0,0 +1,25 @@
+let heightInput = document.querySelector ('#height');
+let weightInput = document.querySelector('#weight');
+let calculate = document.querySelector('#calculate');
+let result = document.querySelector('#answer');
+let statement = document.querySelector('#statement');
+calculate.addEventListener('click', ()=>{
+
+    let height = parseFloat(heightInput.value);
+    let weight = parseFloat(weightInput.value);
+    
+    let BMI = weight / Math.pow(height / 100, 2).toFixed(1);
+    result.innerText = BMI;
+
+    if(BMI < 18.5){
+        statement.innerText = "You're underweight";
+    }else if (BMI > 18.5 && BMI <= 24.9){
+        statement.innerText = "You're healthy";
+    }else if (BMI >= 25 && BMI <= 29.9){
+        statement.innerText = "You're overweight";
+    }else{
+        statement.innerText = "You're obese";
+    }
+})
+
+
